cc7603d6d7381a9e2f53698a2c0404d242043501
Jonathan <jonmgomez@hotmail.com>
2012-10-01 19:17:58 -0500

    Crea imágen a partir de una cadena.

--- imageToString.py
+++ imageToString.py
@@ -13,6 +13,30 @@ img_fuente_default = "img/rojo.jpg"
 img_salida_default = "img/img_binaria.bmp"
 nombre_archivo_bin = "datos.txt" #aqui se almacenan los datos de la imagen en 1's y 0's
 
+def codificarImg(img): #Recibe el string de la imagen modificada
+	p=img.rsplit(',')
+	x=p[0]
+	y=p[1]
+	pv=p[2].rsplit('.')
+	print "imagen de: %s x %s \n"  % (x,y)
+	try:
+		size=int(x),int(y)
+	
+		im=Image.new('RGB', size)
+				
+		for i in range(int(y)):
+			for j in range(int(x)):
+				if pv[i][j]=="1":
+					im.putpixel((j,i),(0,0,0)) #NO ESTA HACIENDO ESTO! SE VA SIEMPRE POR EL ELSE
+				else:
+					im.putpixel((j,i),(20,100,255))
+
+		im.save("j.bmp") 
+
+	except ValueError as details:
+		
+		print details
+	
 def main():
     if(len(sys.argv) > 1):
         img_fuente = sys.argv[1] # primer parametro   #
@@ -57,4 +81,6 @@ def main():
     im.save(img_salida_default) 
     return imageString
 
-main()
+#main()
+codificarImg("10,12,0000011111.0000011111.0000011111.0000011111.0000011111.0000011111.0000011111.0000011111.0000011111.0000011111.0000011111.0000011111.")
+raw_input("")
